5a0b74a7089a7adcf061bad8e4b86887d43bfc26
rapzaDev <rapzadev@gmail.com>
2022-02-14 13:03:29 -0300

    Consuming data & questions of Firebase and render it on room screen

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -1,9 +1,9 @@
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/useAuth';
 
-import { ref, push } from 'firebase/database';
+import { ref, push, onValue } from 'firebase/database';
 import { database } from '../../services/firebase';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -13,8 +13,36 @@ import { RoomCode } from '../../components/RoomCode/RoomCode';
 
 import './styles.scss';
 
+
 type RoomParams = {
     id: string;
+}
+
+type FirebaseQuestionsType = Record<string, { 
+    author: {
+        name: string,
+        avatar: string
+    },
+    content: string,
+    isHighlighted: boolean
+    isAnswered: boolean,
+}>
+
+type FirebaseDataType = {
+    authorId: string;
+    questions: FirebaseQuestionsType;
+    title: string;
+}
+
+type QuestionsType = {
+    id: string,
+    author: {
+        name: string,
+        avatar: string
+    },
+    content: string,
+    isHighlighted: boolean
+    isAnswered: boolean,
 }
 
 function Room() {
@@ -24,7 +52,41 @@ function Room() {
 
     const { user } = useAuth();
 
+    // form elements states
     const [ newQuestion, setNewQuestion ] = useState('');
+    
+    // firebase data states
+    const [ roomTitle, setRoomTitle ] = useState('');
+    const [ questions, setQuestions ] = useState<QuestionsType[]>([]);
+
+    useEffect(() => {
+        const roomRef = ref(database, `rooms/${codeID}`);
+
+        const unsubscribe = onValue( roomRef, (snapshot) => {
+            const data: FirebaseDataType = snapshot.val();
+            const { questions, title } = data;
+
+            const parsedQuestions = Object.entries(questions)
+                .map( ([ key, value ]) => {
+                    return {
+                        id: key,
+                        content: value.content,
+                        author: value.author,
+                        isHighlighted: value.isHighlighted,
+                        isAnswered: value.isAnswered
+                    }
+                });
+            
+            setRoomTitle(title);
+            setQuestions(parsedQuestions);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+
+    }, [codeID]); /** everytime the codeID changes, for any reason, 
+    the useEffect will execute the entire code again and get the new data for the new room. */ 
 
 
     async function handleSendQuestion(event: FormEvent) {
@@ -46,7 +108,8 @@ function Room() {
             isAnswered: false
         }
 
-        const roomRef = ref(database, `rooms/${codeID}/questions`); // creating the questions label on the specify room.
+        // sending the new question to 'questions' label on the specify room.
+        const roomRef = ref(database, `rooms/${codeID}/questions`); 
 
         await push(roomRef, question);
 
@@ -78,8 +141,8 @@ function Room() {
 
             <main className="main-content">
                 <div className="room-title">
-                    <h1>Sala React</h1>
-                    <span>4 perguntas</span>
+                    <h1>Sala {roomTitle}</h1>
+                    { (questions.length > 0) && <span>{questions.length} pergunta(s)</span>}
                 </div>
 
                 <form  onSubmit={handleSendQuestion}>
@@ -89,15 +152,21 @@ function Room() {
                         value={newQuestion}
                     />
                     <div className="form-footer">
+                        
                         { renderUserInfo() }
+
                         <DefaultButton 
                             type="submit"
                             disabled={!user}
                         >
                             Enviar pergunta
                         </DefaultButton>
+
                     </div>
                 </form>
+
+                { JSON.stringify(questions) }
+                
             </main>
         </div>
     );
